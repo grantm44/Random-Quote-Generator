216dc35ff521a022a8bcb7c82b31c57fb85c1229
Grant McDonnal <grantmcdonnal@gmail.com>
2016-09-05 13:38:38 -0700

    Added more quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,5 +1,7 @@
 var quotes = [
 	{quote: "The best way to predict the future is to create it.", source: "Abraham Lincoln"},
 	{quote: "Those who dare to fail miserably can achieve greatly.", source:"John F. Kennedy"},
-	{quote: "You can do anything but not everything", source: "David Allen",citation: "Making it all Work", year: 2009}
+	{quote: "You can do anything but not everything", source: "David Allen",citation: "Making it all Work", year: 2009},
+	{quote: "It is not the mountain we conquer but ourselves.", source: "Edmund Hillary"},
+	{quote: "Life is like a bicycle. To keep your balance you must keep moving.", source:"Albert Einstein",year:1930}
 ]
